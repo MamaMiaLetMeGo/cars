d2a67ea21072956c5831a163a66699974e2408c1
kishan_coder <kishan.sutex.kp@gmail.com>
2020-06-17 18:22:02 +0530

    set car filter by state. & show images.

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -4,13 +4,13 @@ class CarsController < ApplicationController
   before_action :set_car, only: %i[show edit update destroy]
   before_action :set_makes, except: %i[show destroy]
 
-  
+
   def index
-    set_cars_and_make_with_criteria(params[:make], '')
+    set_cars_and_make_with_criteria(params[:make], '', '')
   end
 
   def search
-    set_cars_and_make_with_criteria(params[:make], params[:order])
+    set_cars_and_make_with_criteria(params[:make], params[:order] , params[:state])
   end
 
 
@@ -73,10 +73,10 @@ class CarsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def car_params
-    params.require(:car).permit(:title, :description, :feature, :price, :model, :year, :color, :miles, make_ids: [], state_ids: [])
+    params.require(:car).permit(:title, :description, :feature, :price, :model, :thumbnail, :year, :color, :miles, make_ids: [], state_ids: [])
   end
 
-  def set_cars_and_make_with_criteria(requested_make, requested_order)
+  def set_cars_and_make_with_criteria(requested_make, requested_order, requested_state)
     if requested_make.nil? || requested_make.eql?('All')
       cars_by_make = Car.all
       @make_name = 'All'
@@ -84,34 +84,52 @@ class CarsController < ApplicationController
       cars_by_make = filter_cars_by_make(requested_make)
       @make_name = requested_make
     end
-    order_cars(requested_order, cars_by_make)
+
+    if requested_state == '' || requested_state.nil? || requested_state.eql?('All')
+      cars_by_state = cars_by_make
+    else
+      cars_by_state = filter_cars_by_state(requested_state, cars_by_make)
+    end
+
+    order_cars(requested_order, cars_by_state)
   end
 
   def filter_cars_by_make(make_name)
     @make = Make.find_by(name: make_name)
     cars = if @make.nil?
-              Car.none
-            else
-              @make.cars
-    end
+             Car.none
+           else
+             @make.cars
+           end
+  end
+
+  def filter_cars_by_state(state_name, cars_by_make)
+    @make = State.find_by(state: state_name)
+    cars = if @make.nil?
+             # Car.none
+           else
+             @make.cars.where(id: cars_by_make.pluck(:id))
+
+           end
+
   end
 
   def order_cars(_order, _cars)
     @cars = case _order
-             when 'A-Z'
-               _cars.order('title ASC')
-             when 'Z-A'
-               _cars.order('title DESC')
-             when 'Highest Priced First'
-               _cars.order('price DESC')
-             when 'Lowest Priced First'
-               _cars.order('price ASC')
-             when 'Newest First'
-               _cars.order('created_at DESC')
-             when 'Oldest First'
-               _cars.order('created_at ASC')
-             else
-               _cars.order('title ASC')
-    end
+              when 'A-Z'
+                _cars.order('title ASC')
+              when 'Z-A'
+                _cars.order('title DESC')
+              when 'Highest Priced First'
+                _cars.order('price DESC')
+              when 'Lowest Priced First'
+                _cars.order('price ASC')
+              when 'Newest First'
+                _cars.order('created_at DESC')
+              when 'Oldest First'
+                _cars.order('created_at ASC')
+              else
+                _cars.order('title ASC')
+            end
   end
 end
