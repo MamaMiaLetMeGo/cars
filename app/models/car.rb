304ab6d4a7c4ad5c40e4ecb08bbce0ccf8fb65df
charles <your-gitcommitcg@gmail.com>
2020-07-04 10:11:23 -0400

    fixed footer and navbar

--- app/models/car.rb
+++ app/models/car.rb
@@ -5,6 +5,9 @@ class Car < ApplicationRecord
   has_one_attached :thumbnail
   has_many_attached :images
   has_rich_text :description
+  validates :title, presence: true
+  validates :description, presence: true
+  validate :image_type
   acts_as_votable
 
   def resized_thumbnail
@@ -13,5 +16,17 @@ class Car < ApplicationRecord
 
   def manyimage input
     return self.images[input].variant(resize: '350x250!').processed
+  end
+  
+  private
+  def image_type
+    if images.attached? == false
+      errors.add(:images, "are missing!")
+    end
+    images.each do |image|
+      if !image.content_type.in?(%('image/jpeg image/jpg image/png'))
+        errors.add(:images, 'needs to be a JPEG, JPG or PNG')
+      end
+    end
   end
 end
